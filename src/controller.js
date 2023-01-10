caf12ae9d170167d2b1fae5ed1f937c382f3f634
Anthony Moran <tonymmoran@gmail.com>
2023-01-10 21:08:48 +0000

    added set sail ship move code

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,13 @@
 (function exportController() {
   class Controller {
         constructor() {
-            this.initialiseSea();
             this.ship = ship;
-        }
+            this.initialiseSea();
+
+            document.querySelector('#sailbutton').addEventListener('click', () => {
+              this.setSail();
+            });
+          }
         
         initialiseSea() {
             const backgrounds = [
@@ -37,19 +41,45 @@
         }
 
         renderShip() {
-          // this.ship = ship
-
+          this.ship = ship;
+          
           const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
           const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`); 
+        
           
           const shipElement = document.querySelector('#ship');
           shipElement.style.top = `${portElement.offsetTop + 32}px`;
           shipElement.style.left = `${portElement.offsetLeft - 32}px`;
         }
-    }
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Controller;
-    } else {
+
+        setSail() {
+          this.ship = ship
+
+          const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+          const nextPortIndex = currentPortIndex + 1;
+          const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}"]`); 
+          
+          if (!nextPortElement) {
+            return alert('End of the line Mo Fo!!!!');
+          }
+          const shipElement = document.querySelector('#ship');
+          const sailInterval = setInterval(() => {
+            const shipLeft = parseInt(shipElement.style.left, 10);
+            if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+            
+            ship.setSail();
+            ship.dock();
+            clearInterval(sailInterval);
+          }
+
+          shipElement.style.left = `${shipLeft + 100}px`;
+          }, 20);
+        }
+      }
+  
+    if (typeof module !== 'undefined' && module.exports) {
+      module.exports = Controller;
+      } else {
         window.Controller = Controller;
-    }
+      }
 }());
